fe5c28c5e0c6b63c07a37c30dae8ade344477737
brucewu-fly <brucewu.fly.cn@gmail.com>
2018-01-11 16:06:47 +0800

    Add .travis.yml

--- src/test/java/com/aliyun/openservices/log/log4j/LoghubAppenderTest.java
+++ src/test/java/com/aliyun/openservices/log/log4j/LoghubAppenderTest.java
@@ -9,22 +9,21 @@ import org.junit.Test;
  */
 public class LoghubAppenderTest {
 
-    private static Logger logger = LogManager.getLogger(LoghubAppenderTest.class);
+    private static final Logger LOGGER = LogManager.getLogger(LoghubAppenderTest.class);
 
     @Test
     public void testLogCommonMessage() {
-        logger.warn("This is a test common message logged by log4j.");
+        LOGGER.warn("This is a test common message logged by log4j.");
     }
 
     @Test
     public void testLogThrowable() {
-        logger.error("This is a test error message logged by log4j.",
-                new UnsupportedOperationException("log4j appender test."));
+        LOGGER.error("This is a test error message logged by log4j.",
+                new UnsupportedOperationException("Log4j UnsupportedOperationException"));
     }
 
     @Test
     public void testLogLevelInfo() {
-        logger.info("This is a test error message logged by log4j, level is info,should not be logged.");
+        LOGGER.info("This is a test error message logged by log4j, level is info, should not be logged.");
     }
-
 }
